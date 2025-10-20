bec472c91c445daf1ff174e4d4b75e86c71085e0
Zehan <zehan.kareem@gmail.com>
2025-10-20 12:36:40 +0600

    init(iamzehan): Completed Project

---
+++ test.js
@@ -0,0 +1,64 @@
+export function capitalized(word) {
+  try {
+    return word[0].toUpperCase() + word.slice(1);
+  } catch {
+    throw new Error();
+  }
+}
+
+export function reversedString(string) {
+  return string.split("").reverse().join("");
+}
+
+export const calculator = (function () {
+  const add = (a, b) => a + b;
+  const subtract = (a, b) => a - b;
+  const divide = (a, b) => a / b;
+  const multiply = (a, b) => a * b;
+
+  return {
+    add,
+    subtract,
+    divide,
+    multiply,
+  };
+})();
+
+export function caesarCipher(string, key) {
+  const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+  const lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
+
+  const indexes = []; // keep record of the indexes of the sent string
+  string.split("").forEach((value) => {
+    if (value.match("[a-z]")) indexes.push(lowerCase.indexOf(value));
+    else if (value.match("[A-Z]")) indexes.push(upperCase.indexOf(value) + 100);
+    else indexes.push(value);
+  });
+
+  function shifter(alphabets) {
+    // now shift according to the key
+    const shift = alphabets.splice(0, key);
+    alphabets.push(...shift); // add them to the end of the alphabets
+    return alphabets;
+  }
+
+  shifter(lowerCase);
+  shifter(upperCase);
+
+  indexes.forEach((index, idx) => {
+    if (typeof index == "number") {
+      if (index > 100) indexes[idx] = upperCase[index - 100];
+      else if (index < 100) indexes[idx] = lowerCase[index];
+    }
+  }); // cipher
+  return indexes.join("");
+}
+
+export function analyzeArray(array) {
+  return {
+    average: array.reduce((a, b) => a + b, 0) / array.length,
+    min: Math.min(...array),
+    max: Math.max(...array),
+    length: array.length,
+  };
+}
---
+++ test.test.js
@@ -0,0 +1,42 @@
+import { 
+    capitalized, 
+    reversedString, 
+    calculator, 
+    caesarCipher, 
+    analyzeArray 
+} 
+from "./test";
+
+test("Capitalizes the first letter", () => {
+  expect(capitalized("ziaul")).toBe("Ziaul");
+});
+
+test("Reverses a string", () => {
+  expect(reversedString("String")).toBe("gnirtS");
+});
+
+test("Calculates", () => {
+  expect(calculator.add(1, 2)).toBe(3);
+  expect(calculator.subtract(10, 4)).toBe(6);
+  expect(calculator.divide(4, 2)).toBe(2);
+  expect(calculator.multiply(4, 7)).toBe(28);
+});
+
+test("CaesarCipher", () => {
+  expect(caesarCipher("zehan", 3)).toBe("chkdq");
+  expect(caesarCipher("xyz", 3)).toBe("abc");
+  expect(caesarCipher("HeLLo", 3)).toBe("KhOOr");
+  expect(caesarCipher("Hello, World!", 3)).toBe("Khoor, Zruog!");
+});
+
+test("Analyze array", () => {
+  expect(
+    analyzeArray([1, 8, 3, 4, 2, 6])
+).toEqual({
+      average: 4,
+      min: 1,
+      max: 8,
+      length: 6,
+    })
+  }
+);
