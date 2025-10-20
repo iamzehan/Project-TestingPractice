cfe50729ce4ade09483b4b29820b83712191cb65
Zehan <zehan.kareem@gmail.com>
2025-10-20 13:00:51 +0600

    update(iamzehan): Tests for Decoupled analyzeArray() function

--- test.test.js
+++ test.test.js
@@ -1,11 +1,10 @@
-import { 
-    capitalized, 
-    reversedString, 
-    calculator, 
-    caesarCipher, 
-    analyzeArray 
-} 
-from "./test";
+import {
+  capitalized,
+  reversedString,
+  calculator,
+  caesarCipher,
+  analyzeArray,
+} from "./test";
 
 test("Capitalizes the first letter", () => {
   expect(capitalized("ziaul")).toBe("Ziaul");
@@ -30,13 +29,18 @@ test("CaesarCipher", () => {
 });
 
 test("Analyze array", () => {
-  expect(
-    analyzeArray([1, 8, 3, 4, 2, 6])
-).toEqual({
-      average: 4,
-      min: 1,
-      max: 8,
-      length: 6,
-    })
-  }
-);
+  expect(analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6,
+  });
+});
+
+test("Decoupled Array Analyzer", () => {
+  const obj = analyzeArray([1, 8, 3, 4, 2, 6]);
+  expect(obj.average).toBe(4);
+  expect(obj.min).toBe(1);
+  expect(obj.max).toBe(8);
+  expect(obj.length).toBe(6)
+});
